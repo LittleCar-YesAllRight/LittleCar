663f752737ebdb71b8a45bdd8957e4701a35f456
KudouKaito <lhy634@outlook.com>
2020-07-24 12:15:59 +0800

    添加了差速转向

--- AvoidanceAndIndicate.c
+++ AvoidanceAndIndicate.c
@@ -1,5 +1,6 @@
 #include "reg52.h"
 #define turn_time 1
+#define ABS_NOT(x) (x<0?~x:x)
 // 数字灯
 sbit led_S=P0^1;  // 中间杠
 sbit led_LF=P0^2;  // 左前杠
@@ -22,47 +23,49 @@ sbit rinspection=P3^6;  // 右巡线
 void Delay(unsigned short num);
 void CarRun(unsigned char dire,unsigned char mode);
 void FlashLED();
-int AvoidanceAndIndicate();
+void AvoidanceAndIndicate();
+void CarRunPro(signed char left_rate,signed char right_rate);
 
 int main(void)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
 {
+				
 	AvoidanceAndIndicate();
 	while(1);
 }
 
 // 巡线和避障
-int AvoidanceAndIndicate()
+void AvoidanceAndIndicate()
 {
 	while(1)
 	{
 		// 拿在空中 或者 碰到黑线 就停
-		if(linspection==1 && rinspection==1)
+		if(1==linspection && 1==rinspection)
 		{
 			CarRun('S','T');
 			continue;
 		}
 		// 避障
-		if(lavoidance==1 && ravoidance==1 && linspection==0 && rinspection==0)  // 没有障碍,并且不压线
+		if(1==lavoidance && 1==ravoidance && 0==linspection && 0==rinspection)  // 没有障碍,并且不压线
 			CarRun('F','T');
-		else if(lavoidance==0 && ravoidance==1)  // 左边有障碍
+		else if(0==lavoidance && 1==ravoidance)  // 左边有障碍
 		{
 			CarRun('R','T');
 			Delay(turn_time);
 		}
-		else if(lavoidance==1 && ravoidance==0)  // 右边有障碍
+		else if(1==lavoidance && 0==ravoidance)  // 右边有障碍
 		{
 			CarRun('L','T');
 			Delay(turn_time);
 		}
-		else if(lavoidance==0 && ravoidance==0)  // 前面有障碍
+		else if(0==lavoidance && 0==ravoidance)  // 前面有障碍
 		{
 			CarRun('L','B');
 		}
 		//巡线
-		if(linspection==0 && rinspection==1)
-			CarRun('R','F');
-		else if(linspection==1 && rinspection==0)
-			CarRun('L','F');
+		if(0==linspection && 1==rinspection)
+			CarRunPro(127,-127);
+		else if(1==linspection && 0==rinspection)
+			CarRunPro(-127,127);
 	}
 	
 }
@@ -83,6 +86,9 @@ void Delay(unsigned short num)
         while(temp--);
     }
 }
+/**********************************
+***********************************/
+
 
 /***********************************
 *函数名称：void CarRun(unsigned char dire)
@@ -96,85 +102,60 @@ void CarRun(unsigned char dire,unsigned char mode)
     switch (dire)
     {
         case 'S':             //停止
-						FlashLED();
-            moto_RF=0;
-            moto_RB=0;
-            moto_LF=0;
-            moto_LB=0;
-						led_S=0;
+						CarRunPro(0,0);
             break;
         case 'F':            //前进
-						FlashLED();
-            moto_RF=1;
-            moto_RB=0;
-            moto_LF=1;
-            moto_LB=0;
-						led_F=0;
+            CarRunPro(127,127);
             break;
         case 'B':            //后退 
-						FlashLED();       
-            moto_RF=0;
-            moto_RB=1;
-            moto_LF=0;
-            moto_LB=1;
-						led_B=0;
+            CarRunPro(-128,-128);
 	    break;
         case 'L':            //左转        
-						FlashLED();
-						if(mode=='B')
-							moto_RF=0;
+						if('B'==mode)
+							CarRunPro(-128,0);
+						if('F'==mode)
+							CarRunPro(0,127);
 						else
-						{
-							moto_RF=1;
-							led_LF=0;
-            }
-						moto_RB=0;
-            moto_LF=0;
-						if(mode=='F')
-							moto_LB=0;
-						else
-						{
-							moto_LB=1;
-							led_LB=0;
-						}
+							CarRunPro(-128,127);
 	    break;
         case 'R':            //右转
-						FlashLED();
-            moto_RF=0;
-						if(mode=='F')
-							moto_RB=0;
+            if('B'==mode)
+							CarRunPro(0,-128);
+						if('F'==mode)
+							CarRunPro(127,0);
 						else
-						{
-							moto_RB=1;
-							led_RB=0;
-						}
-						if(mode=='B')
-							moto_LF=0;
-						else
-						{
-							moto_LF=1;
-							led_RF=0;
-						}
-            moto_LB=0;
-	    break;
+							CarRunPro(127,-128);
+						break;
     }
 }
 
+/**********************************
+***********************************/
+
 /***********************************
-*函数名称：void Led(unsigned char dire)
-*函数功能：行进方向控制
-*参数说明：dire 方向 'S'：停止   'F'：前   'B'：后   'L'：左   'R'：右
+*函数名称：void CarRunPro(signed char left_rate,signed char right_rate)
+*函数功能：行进方向的高级控制
+*参数说明：left_rate/right_rate(-128~127)表示左/右轮跑的速度,0/-1表示完全不运动,实际上,只有接近127能驱动
 
 ***********************************/
-void FlashLED()
+void CarRunPro(signed char left_rate,signed char right_rate)
 {
-	led_S=1;//中间杠
-	led_LF=1;//左前杠
-	led_LB=1;//左后杠
-	led_B=1;//后杠
-	led_RB=1;//右后杠
-	led_RF=1;//右前杠
-	led_F=1;//前杠
+	static signed char l=0,r=0;
+	moto_RF=0<right_rate && 0==r;
+	moto_RB=-1>right_rate && 0==r;
+	moto_LF=0<left_rate && 0==l;
+	moto_LB=-1>left_rate && 0==l;
+	led_S=!((0==left_rate||-1==left_rate)&&(0==right_rate||-1==right_rate));// 左右皆止
+	led_LF=!(0<right_rate&&!(0<left_rate));// 右进左转
+	led_LB=!(-1>left_rate&&!(-1>right_rate));// 左退左转
+	led_B=!(-1>left_rate&&-1>right_rate);// 后退
+	led_RB=!(!(-1>left_rate)&&-1>right_rate);// 右退右转
+	led_RF=!(0<left_rate&&!(0<left_rate));// 左进右转
+	led_F=!(0<left_rate&&0<right_rate);// 前进
+	l++;
+	r++;
+	l%=128-ABS_NOT(left_rate);
+	r%=128-ABS_NOT(right_rate);
 }
 /**********************************
 ***********************************/
