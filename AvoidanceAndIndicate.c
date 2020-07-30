7f021f9024bdf2b5873f6c4fdb192b09369fefd3
KudouKaito <lhy634@outlook.com>
2020-07-30 21:48:12 +0800

    complete PWM speed adjustment

--- AvoidanceAndIndicate.c
+++ AvoidanceAndIndicate.c
@@ -2,30 +2,49 @@
 #define ABS_NOT(x) (x<0?~x:x)
 #define digit P0  // 数字灯
 // 马达
-sbit moto_RF=P1^6;  // 右轮向前    
-sbit moto_RB=P1^5;  // 右轮向后
-sbit moto_LF=P1^4;  // 左轮向前
-sbit moto_LB=P1^3;  // 左轮向后
+sbit moto_RF=P1^3;  // 右轮向前    
+sbit moto_RB=P1^2;  // 右轮向后
+sbit moto_LF=P1^1;  // 左轮向前
+sbit moto_LB=P1^0;  // 左轮向后
+sbit ena=P1^4;  // 使能A，用于左轮PWM调速
+sbit enb=P1^5;  // 使能B，用于右轮PWM调速
 // 红外避障串口,有障碍为0
 sbit linfrared=P3^4;  // 左红外
 sbit rinfrared=P3^5;  // 右红外
 // 巡线串口, 压线为0
 sbit llp=P3^7;  // 左巡线
 sbit rlp=P3^6;  // 右巡线
+sbit led1=P0^1;
+
 //void Delay(unsigned short);
+void init_sys(void);
 void CarRunPro(char left_rate,char right_rate);
-void Avoidance();
-void LPatrol();
-void FlashLightStatus();
+void Avoidance(void);
+void FlashLightStatus(void);
+char ldutyfactor=127;
+char rdutyfactor=127;
 
 int main(void)
 {
-    
+    init_sys();
     while(1)
     {
-        LPatrol();
-        FlashLightStatus();
+			CarRunPro(10,10);
+        //Avoidance();
+        //Patrol();
+        
+        //FlashLightStatus();
     }
+}
+
+void init_sys()
+{
+    TMOD=0x01;
+    TH0=0xFF;
+    TL0=0xF6;
+    TR0=1;
+    ET0=1;
+    EA=1;
 }
 
 /***********************************
@@ -41,114 +60,55 @@ void Avoidance()
     else if(0==linfrared && 1==rinfrared)  // 左边有障碍 
         CarRunPro(127,-127);
     else if(1==linfrared && 0==rinfrared)  // 右边有障碍 
-        CarRunPro(127,-127);
+        CarRunPro(-127,127);
     else if(0==linfrared && 0==rinfrared)  // 前面有障碍
         CarRunPro(-127,0);
-
 }
 /**********************************
 ***********************************/
 
-
 /***********************************
-*函数名称：void Avoidance()
-*函数功能：右光感巡线
-*参数说明：无参数
-
-***********************************/
-void LPatrol()
-{
-	//if(1==llp && 1==rlp)  // 如果左光感遇到黑线就停，以防万一，也检测一下右光感是否压在黑线上
-    if(1==llp)  
-    {
-        CarRunPro(0,0);  // 停止运行
-        return;
-    }
-    switch(1==rlp){
-        case 0:  // 右光感是白色
-            CarRunPro(100,0);  // 往右转圈
-            break;
-        case 1:  // 右光感是黑色
-            CarRunPro(0,100);  // 往左转圈
-            break;
-    }
-		/*
-    switch(1==llp){
-        case 1:  // 万一到这里左光感还能是1, 就说明脱轨了, 这时候给他个微调
-            CarRunPro(0,100);  // 往左转圈
-            break;
-    }
-		*/
-}
-/**********************************
-***********************************/
-
-
-/***********************************
-*函数名称:void Delay(unsigned short num)
-*函数功能:延时
-*参数说明:num 延时时间 值最大255
-
-***********************************
-void Delay(unsigned short num)
-{
-    unsigned int temp=0;
-    while(num--)
-    {
-        temp=2000;
-        while(temp--);
-    }
-}
-**********************************
-***********************************/
-
-
-/***********************************
-*函数名称：void CarRunPro(char left_rate,char right_rate)
-*函数功能：行进方向的高级控制
-*参数说明：left_rate/right_rate(-128~127)表示左/右轮跑的速度,数值太小可能会无法驱动,0/-1表示完全不运动
+*函数名称：void CarRunPro()
+*函数功能：PWM控制的小车行驶函数
+*参数说明：left_rate和right_rate为占空比,范围-100~100,0未停止
 
 ***********************************/
 void CarRunPro(char left_rate,char right_rate)
 {
-    static char l=0,r=0;
-    if(l <= ABS_NOT(left_rate)){
-        if(left_rate > 0){
-            moto_LF = 1;
-            moto_LB = 0;
-        }else if(left_rate < -1){
-            moto_LB = 1;
-            moto_LF = 0;
-        }else{
-            moto_LB = 0;
-            moto_LF = 0;
-        }
-    }else{
-        moto_LB = 0;
-        moto_LF = 0;
+    rdutyfactor=right_rate;
+    ldutyfactor=left_rate;
+    // 左轮
+    if (left_rate>0)
+    {
+        moto_LF=1;
+        moto_LB=0;
+    }else if (left_rate<-1)
+    {
+        moto_LF=0;
+        moto_LB=1;
+    }else
+    {
+        moto_LF=0;
+        moto_LB=0;
     }
-    if(r <= ABS_NOT(right_rate)){
-        if(right_rate>0){
-            moto_RF = 1;
-            moto_RB = 0;
-        }else if(right_rate<-1){
-            moto_RB = 1;
-            moto_RF = 0;
-        }else{
-            moto_RB = 0;
-            moto_RF = 0;
-        }
-    }else{
-        moto_RB = 0;
-        moto_RB = 0;
+    // 右轮
+    if (right_rate>0)
+    {
+        moto_RF=1;
+        moto_RB=0;
+    }else if (right_rate<-1)
+    {
+        moto_RF=0;
+        moto_RB=1;
+    }else
+    {
+        moto_RF=0;
+        moto_RB=0;
     }
-    l++;
-    r++;
-    l%=128;
-    r%=128;
 }
 /**********************************
 ***********************************/
+
 
 
 /***********************************
@@ -193,3 +153,21 @@ void FlashLightStatus()
 }
 /**********************************
 ***********************************/
+
+void timer0(void) interrupt 1 using 2
+{
+	static char l=0,r=0;
+	
+    if(l<ldutyfactor)
+			ena=1;
+			else
+				ena=0;
+	if(r<rdutyfactor)
+		enb=1;
+	else
+		enb=0;
+	    TH0=0xFF;
+    TL0=0xF6;
+	l=(l+1)%128;
+	r=(r+1)%128;
+}
